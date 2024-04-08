d162f6e711d15cc38854101687cf623ca91db2f1
geovannamatrix <geovanna.santana@matrixenergia.com>
2024-04-08 00:10:46 -0300

    ajustes nas os endpoints, adicionando @PathVariable

--- src/main/java/org/emporio/sabor/real/api/controller/EstoqueController.java
+++ src/main/java/org/emporio/sabor/real/api/controller/EstoqueController.java
@@ -2,6 +2,7 @@ package org.emporio.sabor.real.api.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.emporio.sabor.real.api.model.dto.EstoqueDTO;
 import org.emporio.sabor.real.api.service.EstoqueService;
@@ -9,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,20 +26,20 @@ public class EstoqueController {
 
     @PostMapping
     public ResponseEntity<EstoqueDTO> salvar(@RequestBody EstoqueDTO estoqueDTO) {
-        var estoque = estoqueService.salvar(estoqueDTO);
+        estoqueService.salvar(estoqueDTO);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/id")
-    public ResponseEntity<EstoqueDTO> findById(Long id) {
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Optional<EstoqueDTO>> findById(@PathVariable Long id) {
         var estoque = estoqueService.findById(id);
-        return ResponseEntity.ok(estoque.orElse(null));
+        return ResponseEntity.ok(estoque);
     }
 
-    @GetMapping("/produto")
-    public ResponseEntity<EstoqueDTO> findByProduto(String produto) {
+    @GetMapping("/produto/{produto}")
+    public ResponseEntity<Optional<EstoqueDTO>> findByProduto(@PathVariable String produto) {
         var estoque = estoqueService.findByProduto(produto);
-        return ResponseEntity.ok(estoque.orElse(null));
+        return ResponseEntity.ok(estoque);
     }
 
     @GetMapping
@@ -46,14 +48,14 @@ public class EstoqueController {
         return ResponseEntity.ok(estoque);
     }
 
-    @GetMapping("/categoria")
-    public ResponseEntity<List<EstoqueDTO>> findByCategoria(String categoria) {
+    @GetMapping("/categoria/{categoria}")
+    public ResponseEntity<List<EstoqueDTO>> findByCategoria(@PathVariable String categoria) {
         var estoque = estoqueService.findByCategoria(categoria);
         return ResponseEntity.ok(estoque);
     }
 
-    @DeleteMapping("/id")
-    public ResponseEntity<Void> deleteById(Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         estoqueService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
