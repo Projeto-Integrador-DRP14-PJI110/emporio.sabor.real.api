16b6b829aefc61306390d021c6ca8633a89aebe4
geovannamatrix <geovanna.santana@matrixenergia.com>
2024-04-23 16:41:59 -0300

    add EnableJpaRepositories

--- src/main/java/org/emporio/sabor/real/api/EmporioSaborRealApplication.java
+++ src/main/java/org/emporio/sabor/real/api/EmporioSaborRealApplication.java
@@ -2,8 +2,10 @@ package org.emporio.sabor.real.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "org.emporio")
+@EnableJpaRepositories(basePackages = "org.emporio.sabor.real.api.repository")
 public class EmporioSaborRealApplication {
 
     public static void main(String[] args) {
