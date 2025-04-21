5d15b22a4dc68d0d971f21aad309c815f423085b
geovannamatrix <2203221@aluno.univesp.br>
2025-04-21 16:15:59 -0300

    feat: create model and dto recovery email

--- src/main/java/org/emporio/sabor/real/api/domain/dto/RecoveryEmailDTO.java
+++ src/main/java/org/emporio/sabor/real/api/domain/dto/RecoveryEmailDTO.java
@@ -1,2 +1,17 @@
-package org.emporio.sabor.real.api.domain.dto;public class RecoveryEmailDTO {
+package org.emporio.sabor.real.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecoveryEmailDTO {
+
+    private String email;
+    private String token;
+    private String subject;
 }
--- src/main/java/org/emporio/sabor/real/api/domain/model/RecoveryEmail.java
+++ src/main/java/org/emporio/sabor/real/api/domain/model/RecoveryEmail.java
@@ -1,2 +1,17 @@
-package org.emporio.sabor.real.api.domain.model;public class RecoveryEmail {
+package org.emporio.sabor.real.api.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecoveryEmail {
+
+    private String email;
+    private String token;
+    private String subject;
 }
