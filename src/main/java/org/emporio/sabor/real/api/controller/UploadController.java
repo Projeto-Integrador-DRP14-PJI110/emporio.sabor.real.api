8e0cd88929d0cdb055a21a62ff3e516fab19fc6e
geovannamatrix <2203221@aluno.univesp.br>
2025-04-21 16:18:43 -0300

    feat: create endpoint upload file

--- src/main/java/org/emporio/sabor/real/api/controller/UploadController.java
+++ src/main/java/org/emporio/sabor/real/api/controller/UploadController.java
@@ -1,2 +1,38 @@
-package org.emporio.sabor.real.api.controller;public class UploadController {
+package org.emporio.sabor.real.api.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.emporio.sabor.real.api.service.UploadService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(value = "api/v1/upload")
+@AllArgsConstructor
+@Validated
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Tag(name = "Upload", description = "Upload API")
+public class UploadController {
+
+    private final UploadService uploadService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Realiza upload de arquivos", description = "Realiza upload de arquivos para salvar produtos no sistema de estoque",
+            tags = {"Upload"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Realiza upload de arquivos")})
+    public ResponseEntity<Void> uploadFile(@Valid @RequestParam(value = "file") MultipartFile file) throws IOException {
+
+        uploadService.upload(file.getBytes());
+        return ResponseEntity.ok().build();
+    }
 }
